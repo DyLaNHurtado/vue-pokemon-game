c741629ee30c62290fecc502e3726c0d04bc2b48
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-10-30 00:51:14 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './css/animation.css'
+import './css/styles.css'
 
 createApp(App).mount('#app')
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>¿Cual es este Pokemon?</h1>
+    <PokemonPicture/>
+    <PokemonOptions/>
+</template>
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+export default {
+    name: 'PokemonPage',
+  components: {
+    PokemonPicture,PokemonOptions
+  }
+}
+</script>
