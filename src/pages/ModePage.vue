7af33ad3d1838308be6fafc329546bbdbb264f9a
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-11-14 17:13:53 +0100

    options sprites

--- src/pages/ModePage.vue
+++ src/pages/ModePage.vue
@@ -17,37 +17,41 @@ import GameMode from '@/components/GameMode.vue';
       return{
         gamemodes:[
           {
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png',
           name:'All-in',
-          description:'All pokemons within',
+          description:'Free for all',
         },{
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
           name:'1st Generation',
           description:'Only pokemons of Kanto',
     
         },{
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/160.png',
           name:'2nd Generation',
           description:'Only pokemons of Jhoto',
         },{
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/254.png',
           name:'3rd Generation',
           description:'Only pokemons of Hoenn',
         },{
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png',
           name:'4th Generation',
           description:'Only pokemons of Sinnoh',
         },{
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/503.png',
           name:'5th Generation',
           description:'Only pokemons of Unova',
         },{
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/658.png',
           name:'6th Generation',
           description:'Only pokemons of Kalos',
         },{
-          uri:'',
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/727.png',
           name:'7th Generation',
+          description:'Only pokemons of Alola',
+        },{
+          uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/887.png',
+          name:'8th Generation',
           description:'Only pokemons of Galar',
         }]
       }
