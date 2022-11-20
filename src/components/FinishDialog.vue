e5cb98369fae89f0f56427f4a70e32e4f6a8e3ca
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-11-20 23:57:41 +0100

    Stats ready!

--- src/components/FinishDialog.vue
+++ src/components/FinishDialog.vue
@@ -1,19 +1,20 @@
 <template>
         <div class="dialog fade-in">
-        <h2>Time out!</h2>
+        <h2>⏱️ Time out!</h2>
         <div class="stats-container">
-            <h3>Stats</h3>
-            <span>{{score}}</span>
-            <span>{{correctCount}}</span>
-            <span>{{totalCount-correctCount}}</span>
-            <span>{{totalCount}}</span>
-            <span>{{timeRate}}</span>
+            <h3>Stats:</h3>
+            <Stat :label="'Score:'" :value="score"/>
+            <Stat :label="'Correct Count:'" :value="correctCount"/>
+            <Stat :label="'Incorrect Count:'" :value="totalCount-correctCount"/>
+            <Stat :label="'Total anwsered Count:'" :value="totalCount"/>
+            <Stat :label="'Average Response Time:'" :value="timeRate"/>
         </div>
         <PanelButtons :display="'finish'" class="panel" style="height: 20%;"/>
     </div>
 </template>
 <script>
 import PanelButtons from './PanelButtons.vue';
+import Stat from './Stat.vue';
 
  export default {
     name: "FinishDialog",
@@ -36,7 +37,7 @@ import PanelButtons from './PanelButtons.vue';
         }
     },
     methods: {},
-    components: { PanelButtons }
+    components: { PanelButtons, Stat }
 }
 </script>
 <style scoped>
@@ -61,14 +62,16 @@ import PanelButtons from './PanelButtons.vue';
         z-index: 100;
     }
     h2{
-        font-size: 1.5em;
+        font-size: 1.7em;
         font-family: 'PixelColeco';
+        
     }
     h3{
-        font-size: 1.2em;
+        font-size: 1.5em;
+        box-shadow: 0px 0px 8px #BBC7A4;
     }
     .stats-container{
         width: 100%;
         height: 70%;
-    }
+    }  
 </style>
